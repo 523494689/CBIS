1bcc8dc87d7b72129da5846b24cb85999fa92d9c
flywaiting <flywaiting@hotmail.com>
2018-07-03 17:07:48 +0800

    补充TrainSearchService类

--- src/main/java/com/cbis/controller/TrainSearchController.java
+++ src/main/java/com/cbis/controller/TrainSearchController.java
@@ -1,6 +1,5 @@
 package com.cbis.controller;
 
-import java.util.Arrays;
 import java.util.List;
 
 import javax.annotation.Resource;
@@ -11,50 +10,18 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.cbis.dao.ScheduleDao;
-import com.cbis.dao.TrainDao;
 import com.cbis.entity.Train;
+import com.cbis.service.TrainSearchService;
 
 @Controller
 @RequestMapping("/search-api")
 public class TrainSearchController {
-	@Resource(name = "trainDao")
-	private TrainDao trainDao;
-	@Resource(name = "scheduleDao")
-	private ScheduleDao scheduleDao;
+	@Resource(name = "trainSearchService")
+	private TrainSearchService trainSearchService;
 
 	@RequestMapping(value = "/trains", method = RequestMethod.GET)
 	@ResponseBody
-	public List<Train> search(@RequestParam(name = "start") String startStation,
-			@RequestParam(name = "stop") String stopStation) {
-		String pattern = startStation + ".*" + stopStation;
-		List<Train> trains = trainDao.queryTrains(pattern);
-
-		for (Train train : trains) {
-			String start = startStation;
-			String stop = stopStation;
-
-			List<String> stationList = Arrays.asList(train.getStations().split("-"));
-
-			// 得到第一个包含start字符串的子项
-			for (String string : stationList) {
-				if (string.matches(start + ".*?")) {
-					start = string;
-					break;
-				}
-			}
-
-			// 得到最后一个包含stop字符串的子项
-			for (int i = stationList.size() - 1; i > -1; i--) {
-				if (stationList.get(i).matches(stop + ".*?")) {
-					stop = stationList.get(i);
-					break;
-				}
-			}
-
-			train.setStart(scheduleDao.querySchedule(train.getTrainId(), start));
-			train.setStop(scheduleDao.querySchedule(train.getTrainId(), stop));
-		}
-		return trains;
+	public List<Train> search(@RequestParam(name = "start") String start, @RequestParam(name = "stop") String stop) {
+		return trainSearchService.getTrains(start, stop);
 	}
 }
